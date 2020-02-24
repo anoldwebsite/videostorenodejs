40374fa2be3f8aaebd7b253193c12cdbd624ca7d
Emil Jan <anoldwebsite@gmail.com>
2020-02-24 16:58:23 +0000

    test for genre with valid id written.

--- tests/integration/genres.test.js
+++ tests/integration/genres.test.js
@@ -11,44 +11,15 @@ const { Genre } = require('../../models/Genre');
 
 let server;
 
-function myFunc(server, serverClosed) {
-    let message =
-        `
-        Number of connections to the server: ${server.connections}
-        Is the server listening for connections?: ${server.listening}
-    `;
-    if (serverClosed) {
-        message =
-            `
-        Line of code server.clos() has been executed.
-        Number of connections to the server: ${server.connections}
-        Is the server listening for connections?: ${server.listening}
-        `
-    }
-    console.log(message);
-}
-
-
 //Test suit for the route /api/genres
 describe('/api/genres', () => {
     //Test suit for getting all genres
     beforeEach(() => { server = require('../../index'); });//Opern the server before each test.
 
     afterEach(async () => {
-        myFunc(server, false);//false means line of code server.close() has not been executed yet.
         await server.close();
-        for (let i = 0; i < 3; i++) {
-            console.log(i + 1);
-            myFunc(server, true);//true means line of code server.close() has been executed. 
-        }
-
         await Genre.deleteMany({});//await Genre.remove({});//deprecated. USe deleteMany();
-        for (let i = 0; i < 3; i++) {
-            console.log(i + 1);
-            myFunc(server, true);//true means line of code server.close() has been executed. 
-        }
-
-    });//shut down the server after each test.
+    });//shut down the connection with the server after each test.
 
     describe('GET /', () => {
         it('should return all genres', async () => {
@@ -58,19 +29,35 @@ describe('/api/genres', () => {
                     { name: 'Genre Two' }
                 ]
             );
-            console.log("**************************************");
-            myFunc(server, false);//false means that the line of code server.close() has not been executed yet.
             const res = await request(server).get('/api/genres');
-            myFunc(server, false);//false means that the line of code server.close() has not been executed yet.
-            console.log("**************************************");
-            expect(res.status).toBe(200);
+            expect(res.status).toBe(200);//200 is status code for ok.
             expect(res.body.length).toBe(2);//2 means two Genre objects in the database. res.body retrns an array.
+            //console.log(typeof (res.body));//Object
+            //console.log(Array.isArray(res.body));//True
             expect(res.body.some(g => g.name === 'Genre One')).toBeTruthy();
             /* (callbackfn: (value: T, index: number, array: readonly T[]) => unknown, thisArg?: any): boolean
                 A function that accepts up to three arguments. The some method calls the callbackfn function for each element in the array until the callbackfn returns a value which is coercible to the Boolean value true, or until the end of the array.
                 Determines whether the specified callback function returns true for any element of an array.
             */
             expect(res.body.some(g => g.name === 'Genre Two')).toBeTruthy();
+        });
+    });
+    describe('GET /:id', () => {
+        it('should return a genre if a valid id is passed', async () => {
+            const genre = new Genre({ name: 'Genre One' });//Creating a genre
+            await genre.save();//Saving the genre to the MongoDB
+            const res = await request(server).get('/api/genres/' + genre._id);
+            expect(res.status).toBe(200);
+            //expect(res.body).toMatchObject(genre); 
+            /* The line above will fail because when mongoose generates property _id but sets it to an ObjectId in the MongoDB, 
+            but when we read the object genre from the MongoDB, the id is a string. So, the id generated originally by mongoose is
+            what the test expects but the one retrieved from the MongoDB is a string, so there is a diference between the 
+            expected and recieved id. Therefore, we will compare the name property of the genre instead. We can also convert the genre._id to string 
+            to compare it to the one retrieved from MongoDB. */
+            expect(res.body).toHaveProperty('name', genre.name);
+            expect(res.body).toHaveProperty('_id', genre._id.toString());
+            //console.log(typeof (res.body));//Object
+            //console.log(Array.isArray(res.body));
         });
     });
 });
